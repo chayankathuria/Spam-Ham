4cccd7fa36a746fc7a0a91a559aebab4958e24eb
Chayan Kathuria <chayankathuria@gmail.com>
2019-11-15 11:51:59 +0530

    Count Vectorizer

---
+++ Vectorizing.py
@@ -0,0 +1,19 @@
+# 6: Generating Features using CountVectorizer
+
+# instantiate the vectorizer
+vect = CountVectorizer()
+
+# learn training data vocabulary, then use it to create a document-term matrix
+vect.fit(X_train)
+
+X_train_dtm = vect.transform(X_train)
+
+# equivalently: combine fit and transform into a single step
+X_train_dtm = vect.fit_transform(X_train)
+
+# examine the document-term matrix
+X_train_dtm
+
+# transform testing data (using fitted vocabulary) into a document-term matrix
+X_test_dtm = vect.transform(X_test)
+X_test_dtm
