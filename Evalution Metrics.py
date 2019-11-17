92cff453b12965f2c51c307b9ae262b8e57e0a0d
Chayan Kathuria <chayankathuria@gmail.com>
2019-11-17 16:59:29 +0530

    Accuracy, Confusion Matrix, AUC/ROC

--- Evalution Metrics.py
+++ Evalution Metrics.py
@@ -21,3 +21,22 @@ X_test[y_test < y_pred_class]
 
 # print message text for the false negatives (spam incorrectly classified as ham)
 X_test[y_test > y_pred_class]
+
+# calculate AUC
+
+# metrics.roc_auc_score(y_test, y_pred_prob)
+print("AUC Score: " + '{:.5f}%'.format(metrics.roc_auc_score(y_test, y_pred_prob)*100))
+
+preds = y_pred_prob_clf[:,1]
+fpr, tpr, threshold = metrics.roc_curve(y_test, preds)
+roc_auc = metrics.auc(fpr, tpr)
+
+plt.title('Receiver Operating Characteristic')
+plt.plot(fpr, tpr, 'b', label = 'AUC = %0.2f' % roc_auc)
+plt.legend(loc = 'lower right')
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0, 1])
+plt.ylim([0, 1])
+plt.ylabel('True Positive Rate')
+plt.xlabel('False Positive Rate')
+plt.show()
